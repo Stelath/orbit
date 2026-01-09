aa0f1e005d2b34398619107690346400d8ce3c43
Alex <korte@clemson.edu>
2026-01-09 18:33:07 -0500

    Initial Commit

---
+++ orbit/orbitApp.swift
@@ -0,0 +1,32 @@
+//
+//  orbitApp.swift
+//  orbit
+//
+//  Created by Alexander Korte on 1/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct orbitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ orbitTests/orbitTests.swift
@@ -0,0 +1,17 @@
+//
+//  orbitTests.swift
+//  orbitTests
+//
+//  Created by Alexander Korte on 1/9/26.
+//
+
+import Testing
+@testable import orbit
+
+struct orbitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
